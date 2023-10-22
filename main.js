8bd068c64b6906cdf4cf4b5eab893b9fa3f03d75
Felipe Lancheros <j.lancherosc@uniandes.edu.co>
2023-10-22 01:05:33 -0500

    compiled

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 import { series } from './data.js';
 document.addEventListener("DOMContentLoaded", function () {
     var tbody = document.querySelector("tbody");
+    var averageText = document.getElementById("average-text");
     if (tbody) {
         series.forEach(function (serie) {
             var row = document.createElement("tr");
-            var imgCell = document.createElement("td");
-            var imgElement = document.createElement("img");
-            imgElement.src = serie.imageUrl;
-            imgElement.alt = serie.name;
-            imgElement.classList.add("serie-img");
-            imgCell.appendChild(imgElement);
-            row.appendChild(imgCell);
             var idCell = document.createElement("td");
             idCell.textContent = serie.id.toString();
             row.appendChild(idCell);
@@ -27,12 +21,9 @@ document.addEventListener("DOMContentLoaded", function () {
         });
         var totalSeasons = series.reduce(function (acc, serie) { return acc + serie.seasons; }, 0);
         var averageSeasons = totalSeasons / series.length;
-        var averageRow = document.createElement("tr");
-        var averageCell = document.createElement("td");
-        averageCell.colSpan = 5;
-        averageCell.textContent = "El promedio de temporadas de todas las series es ".concat(averageSeasons.toFixed(2));
-        averageRow.appendChild(averageCell);
-        tbody.appendChild(averageRow);
+        if (averageText) {
+            averageText.textContent = "El promedio de temporadas de todas las series es ".concat(averageSeasons.toFixed(2));
+        }
     }
     else {
         console.error("No se encontró el elemento tbody");
